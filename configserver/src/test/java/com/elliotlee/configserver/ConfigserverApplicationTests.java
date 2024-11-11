33dd09861dfc9b9ce3dccb4848669e4794399f97
Elliot Lee <elliotleellsa@gmail.com>
2024-11-11 20:13:52 +0800

    Initialize config server module

---
+++ configserver/src/test/java/com/elliotlee/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elliotlee.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
